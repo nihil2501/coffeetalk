606afdb50c06bc0832ab985b032a2d508229d778
Oren Mittman <nihil2501@gmail.com>
2013-03-26 19:41:03 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeetalk::Application.config.secret_token = 'b07bc7d9da0f240f220fedcedb47bf468e5e7a3a667441092a4664ec453249e411ad81c23dd49a5213d597f8fec37d1789b74638bd3aa2af78773e2c66a79c20'
