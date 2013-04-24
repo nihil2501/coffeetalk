75f4f6b5bdc38529cda98a2fc58b7476f8c82dc5
Oren Mittman <nihil2501@gmail.com>
2013-04-23 21:16:01 -0400

    crazy refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,59 @@
 module ApplicationHelper
-  def submit_button_to(text, object, options = {})
-    link_to text, object, options.merge(class: "btn #{options[:primary] ? 'btn-primary' : ''}")
-  end
+  def organization_accordion_group(organization, options = {})
+    accordion_group(organization.name, '', :"data-parent" => "#organization-accordion", class: options[:active] ? 'active' : '') do
 
-  def heading_link_to(text, url, options = {})
-    content_tag :h2 do
-      link_to(text, url, options)
     end
   end
 
   def accordion_group(text, url, options = {})
-    content_tag :div, class: "accordion-group #{options[:class]}" do
+    content_tag :div, class: "accordion-group #{options[:"accordion-group"][:class]}" do
       content_tag :div, class: "accordion-heading" do
+        link_attributes = { class: "accordion-toggle" }
+
+        extra_attributes = {
+          :"data-toggle" => "collapse",
+          :"data-parent"
+        }
+
         link_to text, url, class: "accordion-toggle"
       end
+
+      yield
     end
+  end
+
+  def accordion_group_link(text, url, options = {})
+    accordion_group(options) do
+      accordion_heading_link(text, url, options = {})
+    end
+  end
+
+  def accordion(options = {})
+    content_tag :div, class: "accordion", id: "#{options[:id]}" do
+      accordion_group(accordion: options)
+    end
+  end
+
+  def accordion_group(options = {})
+    content_tag :div, class: "accordion-group #{options[:class]}" do
+      accordion_heading(accordion_group: options)
+    end
+  end
+
+  def accordion_heading(options = {})
+    content_tag :div, class: "accordion-heading" do
+      accordion_toggle(accordion_heading: options)
+    end
+  end
+
+  def accordion_toggle(options = {})
+    link_to text, url, {
+      class: "accordion-toggle",
+      :"data-toggle" => "collapse",
+      :"data-parent" => options[:accordion_heading][:accordion_group][:accordion][:id]
+    }
+  end
+
+  def accordion_body
   end
 end
