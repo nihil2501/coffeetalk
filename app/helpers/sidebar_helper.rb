c0ac06aa1b8964fd0930f11a5267f15506c41fb5
Oren Mittman <nihil2501@gmail.com>
2013-04-26 01:22:12 -0400

    content_for causing duplicate renders

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -1,18 +1,21 @@
 module SidebarHelper
   def edit_organization_li_link(organization)
-    content_for(:edit_organization_li_link) ||
-    render(partial: 'shared/sidebar/edit_organization', locals: { organization: organization, active: false })
+    provided = content_for(:edit_organization_li_link)
+    default  = render(
+      partial: 'shared/sidebar/edit_organization',
+      locals: { organization: organization, active: false }
+    )
+
+    provided.blank? ? default : provided
   end
 
   def new_group_li_link(organization)
-    content_for(:new_group_li_link) ||
-    render(partial: 'shared/sidebar/new_group', locals: { organization: organization, active: false })
-  end
+    provided = content_for(:new_group_li_link)
+    default  = render(
+      partial: 'shared/sidebar/new_group',
+      locals: { organization: organization, active: false }
+    )
 
-  def group_li_links(organization)
-    content_for(:group_li_links) ||
-    organization.groups.each do |group|
-      concat render(partial: 'shared/sidebar/group', locals: { group: group, active: false })
-    end
+    provided.blank? ? default : provided
   end
 end
